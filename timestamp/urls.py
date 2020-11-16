b5813335e437f1332d1f545ff5f3270ee2a68ed6
Sumesh Pradhan <sumesh.1rn14cs107@gmail.com>
2020-11-16 19:38:53 +0530

    integrated swagger ui with drf-yasg libraray

--- timestamp/urls.py
+++ timestamp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.time_data_now),
-    path('<str:ts>', views.time_data),
+      path('<str:ts>', views.time_data),
     
 ]
